e3b20898e8b31204f5d2d497b33d82f5231123e3
ayatakesi <ayanokoji.takesi@gmail.com>
2016-06-25 01:12:22 +0900

    翻訳対象の退避とPOファイルの初期化

---
+++ initialize-po.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+TEXIS_FOR_TRANSLATE="\
+		   abbrevs.texi \
+		   anti.texi \
+		   arevert-xtra.texi \
+		   basic.texi \
+		   buffers.texi \
+		   building.texi \
+		   cal-xtra.texi \
+		   calendar.texi \
+		   cmdargs.texi \
+		   commands.texi \
+		   custom.texi \
+		   dired-xtra.texi \
+		   dired.texi \
+		   display.texi \
+		   emacs-xtra.texi \
+		   emacs.texi \
+		   emerge-xtra.texi \
+		   entering.texi \
+		   files.texi \
+		   fixit.texi \
+		   fortran-xtra.texi \
+		   frames.texi \
+		   help.texi \
+		   indent.texi \
+		   killing.texi \
+		   kmacro.texi \
+		   m-x.texi \
+		   macos.texi \
+		   maintaining.texi \
+		   mark.texi \
+		   mini.texi \
+		   misc.texi \
+		   modes.texi \
+		   msdog-xtra.texi \
+		   msdog.texi \
+		   mule.texi \
+		   package.texi \
+		   picture-xtra.texi \
+		   programs.texi \
+		   regs.texi \
+		   rmail.texi \
+		   screen.texi \
+		   search.texi \
+		   sending.texi \
+		   text.texi \
+		   trouble.texi \
+		   vc-xtra.texi \
+		   vc1-xtra.texi \
+		   windows.texi \
+		   xresources.texi"
+
+for TEXI in ${TEXIS_FOR_TRANSLATE}
+do
+    # 初回は元ファイルを*.origで退避
+    if [ ! -f ${TEXI} ]
+    then
+	cp -p ${TEXI} ${TEXI}.orig
+    fi
+
+    # POファイルの作成
+    po4a-gettextize -M utf8 -f texinfo -m ${TEXI}.orig -p ${TEXI}.po
+done
